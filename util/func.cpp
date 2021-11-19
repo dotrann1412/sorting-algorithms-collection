cb2423c424f85103d9cb78776adf886bf7e212fd
dotrann1412 <dotrann.1412@gmail.com>
2021-11-19 10:40:12 +0700

    more helpful function

--- util/func.cpp
+++ util/func.cpp
@@ -1,9 +1,26 @@
 #include "func.h"
+#include <fstream>
 
 void _replace(std::string& s, char src, char dest) {
-    //std::string t = "";
-    //for(char x: s) if(x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z' || x == '_') t += x;
-    for(char& c: s) 
-        if(c == src) c = dest;
-    //s = t;
+	for(char& c: s) 
+		if(c == src) c = dest;
+}
+
+int* get_array(std::string file) {
+
+	std::fstream f(file.c_str(), std::ios::in);
+	
+	int n; f >> n;
+	int* a = new int[n];
+	for(int i = 0; i < n; f >> a[i], ++i);
+	f.close();
+	return a;
+}
+
+void toLower(std::string& s) {
+	for(auto& x: s) if(x >= 'A' && x <= 'Z') x += 32;
+}
+
+void toUpper(std::string& s) {
+	for(auto& x: s) if(x >= 'a' && x <= 'z') x -= 32;
 }
--- util/func.h
+++ util/func.h
@@ -5,19 +5,25 @@
 
 template <class T>
 void _swap(T& a, T& b) {
-    T c = a; a = b, b = c;
+	T c = a; a = b, b = c;
 }
 
 template <class T>
 T _min(T a, T b) {
-    return a < b ? a : b;
+	return a < b ? a : b;
 }
 
 template <class T>
 T _max(T a, T b) {
-    return a > b ? a : b;
+	return a > b ? a : b;
 }
 
 void _replace(std::string&, char, char);
+
+int* get_array(std::string file);
+
+void toLower(std::string& s);
+
+void toUpper(std::string& s);
 
 #endif
