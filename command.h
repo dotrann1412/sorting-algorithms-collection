0ecc9f820432499bf0aebf9a496b20d0fc802adc
dotrann1412 <dotrann.1412@gmail.com>
2021-11-19 20:57:47 +0700

    rewrite all command as clearly

---
+++ command.h
@@ -0,0 +1,34 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "./util/timer.h"
+#include "./util/func.h"
+#include "./util/test_generator.h"
+#include "./sorting_algorithms/sort.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#include <map>
+#include <vector>
+#include <string>
+
+void generate_dataset(string folder);
+
+void load_mapping_data(vector<pair<string, string>>& data_file, vector<string>& algo);
+
+void statistic();
+
+void help();
+
+void comparison_mode(vector<string>& command);
+
+void algorithms_mode(vector<string>& command);
+
+void query_processes(vector<string>& command);
+
+void generate_dataset(string folder);
+
+#endif
