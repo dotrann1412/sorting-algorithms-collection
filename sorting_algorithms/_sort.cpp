633f39b9fd0bc7ca880babfced54967fe59bc160
dotrann1412 <dotrann.1412@gmail.com>
2021-11-20 10:33:12 +0700

    change comparison statistical method

--- sorting_algorithms/_sort.cpp
+++ sorting_algorithms/_sort.cpp
@@ -9,19 +9,16 @@
  */
 
 void selection_sort(int* begin, int* end, long long& cmpCount) {
-	cmpCount += end - begin - 1;
-	for (int* i = begin; i < end - 1; ++i) {
+	for (int* i = begin; ++cmpCount && i < end - 1; ++i) {
 		int* select = i;
-		cmpCount += 2 * (end - i - 1);
-		for(int* j = i + 1; j < end; ++j)
-			if(*select > *j) select = j;
+		for(int* j = i + 1;++cmpCount &&  j < end; ++j)
+			if(++cmpCount && *select > *j) select = j;
 		_swap(*select, *i);
 	}
 }
 
 void insertion_sort(int* begin, int* end, long long& cmpCount) {
-	cmpCount += end - begin - 1;
-	for (int* i = begin + 1; i < end; ++i) {
+	for (int* i = begin + 1; ++cmpCount && i < end; ++i) {
 		int hold = *i, *j = i;
 		for (;++cmpCount && j > begin && ++cmpCount && hold < *(j - 1); --j) 
 			*j = *(j - 1);
@@ -33,9 +30,8 @@ void insertion_sort(int* begin, int* end, long long& cmpCount) {
 void bubble_sort(int* begin, int* end, long long& cmpCount) {
 	for(int* i = end - 1; ++cmpCount && i > begin; --i) {
 		int* lastswapped = begin - 1;
-		cmpCount += 2 * (i - begin);
-		for(int* j = begin; j < i; ++j) {
-			if(*j > *(j + 1)) 
+		for(int* j = begin;++cmpCount && j < i; ++j) {
+			if(++cmpCount && *j > *(j + 1)) 
 				_swap(*j, *(j + 1)), lastswapped = j + 1;
 		}
 
@@ -50,16 +46,14 @@ void shaker_sort(int* begin, int* end, long long& cmpCount) {
 	while(++cmpCount && begin < end) {
 		int *lastSwapped = begin;
 		if(++cmpCount && flag) {
-			cmpCount += 2 * (end - begin - 1);
-			for(int* i = begin; i < end - 1; ++i)
-				if(*i > *(i + 1)) _swap(*i, *(i + 1)), lastSwapped = i + 1;
+			for(int* i = begin;++cmpCount && i < end - 1; ++i)
+				if(++cmpCount && *i > *(i + 1)) _swap(*i, *(i + 1)), lastSwapped = i + 1;
 			
 			if(++cmpCount && lastSwapped == begin) break;
 			end = lastSwapped;
 		} else {
-			cmpCount += 2 * (end - begin - 1);
-			for(int* i = end - 1; i > begin; --i)
-				if(*i < *(i - 1)) _swap(*i, *(i - 1)), lastSwapped = i;
+			for(int* i = end - 1;++cmpCount && i > begin; --i)
+				if(++cmpCount && *i < *(i - 1)) _swap(*i, *(i - 1)), lastSwapped = i;
 
 			if(++cmpCount && lastSwapped == begin) break;
 			begin = lastSwapped;
@@ -76,8 +70,7 @@ void shell_sort(int* begin, int* end, long long& cmpCount) {
 	for(int gap = n / 2; ++cmpCount && gap; gap /= 2) {
 		//use insertion with step = gap to sort all sublist of array using insertion sort
 		//idx: [0..10], gap = 2: -> 2 sublists: 0 2 4 6 8 10 and 1 3 5 7 9
-		cmpCount += end - (begin + gap);
-		for(int* i = begin + gap; i < end; ++i) {
+		for(int* i = begin + gap;++cmpCount && i < end; ++i) {
 			int hold = *i, *j = i;
 			for(;++cmpCount && j >= begin + gap && ++cmpCount &&  hold < *(j - gap); j -= gap)
 				*j = *(j - gap);
@@ -92,9 +85,8 @@ void heapify(int* a, int n, int k, long long& cmpCount) {
 	if(++cmpCount && 2 * k >= n) return;
 	int i = 2 * k + 1;
 
-	cmpCount += 4;
-	if(i < n - 1 && a[i] < a[i + 1]) ++i;
-	if(i < n && a[k] < a[i]) _swap(a[i], a[k]);
+	if(++cmpCount && i < n - 1 && ++cmpCount && a[i] < a[i + 1]) ++i;
+	if(++cmpCount && i < n && ++cmpCount && a[k] < a[i]) _swap(a[i], a[k]);
 	
 	heapify(a, n, i, cmpCount);
 }
@@ -103,12 +95,10 @@ void heap_sort(int* a, int* end, long long& cmpCount) {
 	int n = end - a;
 	//this step used to build max heap
 	//we start from n / 2 because half of the last array is a natural heap
-	cmpCount += n / 2;
-	for(int i = n / 2 - 1; i >= 0; --i)
+	for(int i = n / 2 - 1;++cmpCount && i >= 0; --i)
 		heapify(a, n, i, cmpCount);
 	
-	cmpCount += n - 1;
-	for(int i = n - 1; i > 0; --i) {
+	for(int i = n - 1;++cmpCount && i > 0; --i) {
 		_swap(a[i], a[0]);
 		heapify(a, i, 0, cmpCount);
 	}
@@ -121,18 +111,13 @@ void heap_sort(int* a, int* end, long long& cmpCount) {
 //suppose that the array with exactly 1 element is always sorted
 void merge(int* array, int* a, int n, int* b, int m, long long& cmpCount) {
 	int i = 0, j = 0;
-	while(i < n && j < m)
-		if(a[i] < b[j]) array[i + j] = a[i], ++i;
+	while(++cmpCount && i < n && ++cmpCount && j < m)
+		if(++cmpCount && a[i] < b[j]) array[i + j] = a[i], ++i;
 		else array[i + j] = b[j], ++j;
 
-	cmpCount += 2 * (i + j) + 2 * (n - i) + 2 * (m - j);
-
-	while (i < n || j < m)
-		if(i < n) array[i + j] = a[i], ++i;
-		else array[i + j] = b[j], ++j;
+	while(++cmpCount && i < n) array[i + j] = a[i], ++i;
+	while(++cmpCount && j < m) array[i + j] = b[j], ++j;
 }
-
-
 
 void merge_sort(int* begin, int* end, long long& cmpCount) {
 	int n = end - begin; //size of array
@@ -156,10 +141,8 @@ int* partition(int* begin, int* end, long long& cmpCount) {
 	_swap(*(end - 1), *(begin + rand() % (end - begin - 1) + 1));
 
 	int *iter = begin - 1, *pivot = end - 1;
-
-	cmpCount += (end - begin - 1) * 2;
-	for(int* i = begin; i < end - 1; ++i)
-		if(*i < *pivot) ++iter, _swap(*iter, *i);
+	for(int* i = begin;++cmpCount && i < end - 1; ++i)
+		if(++cmpCount && *i < *pivot) ++iter, _swap(*iter, *i);
 
 	++iter, _swap(*iter, *pivot);
 	return iter;
@@ -180,24 +163,20 @@ void quick_sort(int* begin, int* end, long long& cmpCount) {
 void counting_sort(int* begin, int* end, long long& cmpCount) {
 	int mn = *begin, mx = *begin;
 
-	cmpCount += 3 * (end - begin);
-	for (int* i = begin; i < end; ++i) //1 comparison here
-		mn = _min(mn, *i), mx = _max(mx, *i); //2 comparison here
+	for (int* i = begin; ++cmpCount && i < end; ++i)
+		mn = _min(mn, *i), mx = _max(mx, *i), ++cmpCount, ++cmpCount;//2 comparison here
 	
 	//_count not count because count was declared as a function in many librarys
 	int* _count = new int[mx - mn + 1]; 
 	memset(_count, 0x00, (mx - mn + 1) * sizeof(int));
 
-	cmpCount += end - begin;
-	for (int* i = begin; i < end; ++i) 
+	for (int* i = begin;++cmpCount && i < end; ++i) 
 		_count[*i - mn]++;
 
-	cmpCount += mx - mn;
-	for (int i = 1; i <= mx - mn; ++i)
+	for (int i = 1;++cmpCount && i <= mx - mn; ++i)
 		_count[i] += _count[i - 1];
 
-	cmpCount += mx - mn + 1;
-	for (int i = mx - mn; i >= 0; --i) {
+	for (int i = mx - mn;++cmpCount && i >= 0; --i) {
 		while(++cmpCount &&  (_count[i]--) > ((++cmpCount && !i) ? 0: _count[i - 1])) 
 			*(begin + _count[i]) = i + mn;
 	}
@@ -210,17 +189,15 @@ void counting_sort(int* begin, int* end, long long& cmpCount) {
 //->dont need to define another function to get the k'th ditgit of number, ...
 //just use to sort the positive integer array
 void radix_sort(int* begin, int* end, long long& cmpCount, int k = 30) {
-	cmpCount += 2;
-	if(k < 0 || end - begin <= 1) return;
+	if(++cmpCount && k < 0 || ++cmpCount && end - begin <= 1) return;
 
 	//arrange all element with k'th bit = 0 to the left side
 	//then use recursion to sort all the element in the left side, and the right side
 	//base on (k - 1)'th bit until k return to zero or the array's size have 
 	//exactly 0 or 1 element
 	int* iter = begin - 1;
-	cmpCount += 2 * (end - begin);
-	for(int* i = begin; i < end; ++i)
-		if (!(*i >> k & 1)) _swap(*++iter, *i); 
+	for(int* i = begin;++cmpCount && i < end; ++i)
+		if (++cmpCount && !(*i >> k & 1)) _swap(*++iter, *i); 
 
 	radix_sort(begin, ++iter, cmpCount, k - 1);
 	radix_sort(iter, end, cmpCount, k - 1);
@@ -228,8 +205,7 @@ void radix_sort(int* begin, int* end, long long& cmpCount, int k = 30) {
 
 void radix_sort(int* begin, int* end, long long& cmpCount) {
 	int rmsb = 0; //rmsb: right most set bit
-	cmpCount += end - begin;
-	for(int* i = begin; i < end; ++i)
+	for(int* i = begin;++cmpCount && i < end; ++i)
 		for(int j = 30; ++cmpCount && j >= 0; --j)
 			if(++cmpCount && *i >> j & 1) {
 				//if k'th bit of number *i is 1 -> check and break
@@ -249,8 +225,7 @@ void flash_sort(int* begin, int* end, long long& cmpCount) {
 	//Mr.Phuong said 0.43 is the best value for this algorithm
 	int layers_count = 0.43 * (end - begin);
 	
-	cmpCount += 3 * (end - begin);
-	for(int* i = begin; i < end; ++i)
+	for(int* i = begin;++cmpCount && i < end; ++i)
 		mx = _max(*i, mx), mn =  _min(mn, *i);
 	
 	int* layer = new int[layers_count];
@@ -262,13 +237,11 @@ void flash_sort(int* begin, int* end, long long& cmpCount) {
 	};
 	
 	//statistic how many element 
-	cmpCount += end - begin;
-	for(int* i = begin; i < end; ++i)
+	for(int* i = begin;++cmpCount && i < end; ++i)
 		layer[level(*i)]++;
 
 	//find position of last element of each layer
-	cmpCount += layers_count - 1;
-	for(int i = 1; i < layers_count; ++i) 
+	for(int i = 1;++cmpCount && i < layers_count; ++i) 
 		layer[i] += layer[i - 1];
 	
 	int *idx = begin;
