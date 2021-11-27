a49c6c5341a4d04fa858a1e04bc59767b5a941ad
dotrann1412 <dotrann.1412@gmail.com>
2021-11-27 20:41:29 +0700

    run statistic as multiprocessing

---
+++ statistic.bat
@@ -0,0 +1,5 @@
+start "" /realtime main --statistic ./statistic_config/1
+start "" /realtime main --statistic ./statistic_config/2
+start "" /realtime main --statistic ./statistic_config/3
+start "" /realtime main --statistic ./statistic_config/4
+start "" /realtime main --statistic ./statistic_config/5
