5328767e2cc2767813aeba01532e05caf1eecfef
dotrann1412 <dotrann.1412@gmail.com>
2023-01-29 13:05:02 +0700

    runner

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ -std=c++17 -o main main.cpp ./utils/timer.cpp ./sorting_algorithms/sort.cpp  ./sorting_algorithms/_sort.cpp
-main
+g++ -std=c++17 -o main main.cpp ./utils/timer.cpp ./utils/helpers.cpp ./utils/test_generator.cpp ./algorithms/sort.cpp ./utils/jsoncpp/jsoncpp.cpp -I./utils/jsoncpp
+./main
